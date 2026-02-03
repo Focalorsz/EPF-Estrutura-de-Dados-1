45193067257854c082f6f984dd2416554bfbe3d0
Luana <luana.comfort@gmail.com>
2026-02-03 17:34:51 -0300

    Corrige erros e adiciona busca/remove clientes

--- funcoes.h
+++ funcoes.h
@@ -1,17 +1,16 @@
 #ifndef FUNCOES_H
-#define funcoes.h
+#define FUNCOES_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
-#include <ncurses.h>
 #include <stdbool.h>
 #include "structs.h"
 void cadastrarCliente(Cliente **lista);
 void listarClientes(Cliente *lista);
-Cliente* buscarCliente(Cliente *lista, char cpf[]);
+void buscarCliente(Cliente *lista, char cpf[]);
 void editarCliente(Cliente *lista);
 void removerCliente(Cliente **lista);
 void menuPrincipal(Cliente **listaClientes);
